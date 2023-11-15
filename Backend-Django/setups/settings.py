b6be179daf23b52b5c16714b64b801416c07b119
Mohammad Ariful Islam <arifulislamaiub2014@gmail.com>
2023-11-15 15:44:20 -0400

    Package refactored

--- Backend-Django/setups/settings.py
+++ Backend-Django/setups/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'FitnessApp.urls'
+ROOT_URLCONF = 'setups.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'FitnessApp.wsgi.application'
+WSGI_APPLICATION = 'setups.wsgi.application'
 
 
 # Database
